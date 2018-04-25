c6a7b2c02e3966c3bf983150bcaecc4aafd8d466
Theo Markkus Paul <theo.markkus.paul@gmail.com>
2018-04-25 21:56:13 +1200

    Wha happen: * Set EJS to be default view engine for application * Add basic page for switcheroo route

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const ejs = require('ejs');
 const app = express();
 
-app.get('/switcheroo', (req, res) => res.send('hello world'));
+app.set('view engine', 'ejs');
+
+app.get('/switcheroo', (req, res) => res.render('switcheroo'));
 
 app.listen(3000, () => console.log('App is listening on localhost:3000'));
